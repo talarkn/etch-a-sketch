3d67f70a2ba73fb3f1f96fc177a6de586d08ca2f
talarkn <talar.kijilian19@gmail.com>
2023-02-15 09:35:00 +0100

    Add the for loop and the mouseover event listener within the button's click event listener block. Add condition: the foor loops only run if the squares are less than 100. Else the user is promted again to input another number.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const body = document.querySelector('body');
 const gridContainer = document.querySelector('.grid-container');
+const btn = document.querySelector('button');
 
-for (let i = 0; i < 16; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.classList.toggle('grid-row');
-    gridContainer.appendChild(gridRow);
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        gridRow.appendChild(div);
-        div.classList.toggle('grid');
+let sqrs = 0;
+
+btn.addEventListener('click', () => {
+    sqrs = Number(prompt('How many squares?'));
+
+    if (sqrs < 100) {
+        for (let i = 0; i < sqrs; i++) {
+            const gridRow = document.createElement('div');
+            gridRow.classList.toggle('grid-row');
+            gridContainer.appendChild(gridRow);
+            for (let j = 0; j < sqrs; j++) {
+                const div = document.createElement('div');
+                gridRow.appendChild(div);
+                div.classList.toggle('grid');
+            }
+        }
     }
-}
+    else {
+        prompt('Cannot input a number higher than 100. Try again!');
+    }
 
-const grids = document.querySelectorAll('.grid');
-
-grids.forEach(grid => grid.addEventListener('mouseover', () => grid.classList.add('grid-color')));
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseover', () => grid.classList.add('grid-color')));
+});
