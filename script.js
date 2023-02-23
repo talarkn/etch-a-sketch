6d07c8f3f7c647e85a403388f33a0fb4be5b3c8b
talarkn <talar.kijilian19@gmail.com>
2023-02-23 09:05:44 +0100

    Add clear function and button event
    Create function: clear, to empty the grid container. Remove the child
    nodes of the grid container once the clear button is clicked. Set the
    number of squares back to zero this way user can recreate a new grid
    size.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const body = document.querySelector('body');
+let gridRow;
+let grid;
 const gridContainer = document.querySelector('.grid-container');
-const btn = document.querySelector('button');
+const sqrBtn = document.querySelector('.sqr-nbr');
+const clearBtn = document.querySelector('.clear');
 
 let sqrs = 0;
 
-btn.addEventListener('click', () => {
+sqrBtn.addEventListener('click', () => {
+    if (sqrs === 0 || sqrs > 100) {
     sqrs = Number(prompt('How many squares?'));
-
+    
     if (sqrs < 100) {
         for (let i = 0; i < sqrs; i++) {
-            const gridRow = document.createElement('div');
+            gridRow = document.createElement('div'); // add these outside the for loops? Before them?
             gridRow.classList.toggle('grid-row');
             gridContainer.appendChild(gridRow);
             for (let j = 0; j < sqrs; j++) {
-                const div = document.createElement('div');
-                gridRow.appendChild(div);
-                div.classList.toggle('grid');
+                grid = document.createElement('div');
+                gridRow.appendChild(grid);
+                grid.classList.toggle('grid');
             }
         }
     }
@@ -25,4 +29,15 @@ btn.addEventListener('click', () => {
 
     const grids = document.querySelectorAll('.grid');
     grids.forEach(grid => grid.addEventListener('mouseover', () => grid.classList.add('grid-color')));
-});
+
+}});
+
+clearBtn.addEventListener('click', clear);
+
+function clear() {
+    if (sqrs > 0) {
+        const gridRows = document.querySelectorAll('.grid-row');
+        gridRows.forEach(gridRow => gridContainer.removeChild(gridRow));
+    }
+    sqrs = 0;
+}
