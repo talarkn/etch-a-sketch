59d42c8ac4849d4ee3bac9ed572f771965cfd11d
talarkn <talar.kijilian19@gmail.com>
2023-02-25 09:30:03 +0100

    Change colorGrid function, add new button features
    Change colorGrid function from adding a class to each grid to adding the
    background color inline.
    Create getColor function which returns a random color.
    Create random and eraser variables. Change background color of
    grid to a random color using the getColor function every time Random
    Color button is clicked. Change background color of grid back to white
    every time Eraser is clicked.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const body = document.querySelector('body');
-let gridRow;
-let grid;
 const gridContainer = document.querySelector('.grid-container');
 const sqrBtn = document.querySelector('.sqr-nbr');
 const clearBtn = document.querySelector('.clear');
 const blackWhite = document.querySelector('.bw');
-const rainbow = document.querySelector('.rainbow');
+const randomColor = document.querySelector('.random');
+const eraser = document.querySelector('.eraser');
 
+let gridRow;
+let grid;
 let sqrs = 0;
 
 function clear() {
@@ -17,9 +18,18 @@ function clear() {
     sqrs = 0;
 }
 
-function colorGrid(aClass) {
+function colorGrid(aColor) {
     const grids = document.querySelectorAll('.grid');
-    grids.forEach(grid => grid.addEventListener('mouseover', () => grid.classList.add(aClass)));
+    grids.forEach(grid => grid.addEventListener('mouseover', () => grid.style.backgroundColor = aColor));
+}
+
+function getColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 sqrBtn.addEventListener('click', () => {
@@ -43,7 +53,7 @@ sqrBtn.addEventListener('click', () => {
     }
 }});
 
-blackWhite.addEventListener('click', () => colorGrid('grid-black') )
-// rainbow.addEventListener('click', () => colorGrid('rainbow'));
-
+blackWhite.addEventListener('click', () => colorGrid('black'));
+randomColor.addEventListener('click', () => colorGrid(getColor()));
+eraser.addEventListener('click', () => colorGrid('white'));
 clearBtn.addEventListener('click', clear);
